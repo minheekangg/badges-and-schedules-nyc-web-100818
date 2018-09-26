c8f57da4d4e01920c766eb334d158a73f49131c3
Min Hee  Park <minheekangg@gmail.com>
2018-09-26 02:40:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,10 +3,9 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-list_of_badges = []
 def batch_badge_creator(arr)
   arr.each do |name|
-    list_of_badges << badge_maker(name)
+    arr = badge_maker(name)
   end
   return list_of_badges
 end
