1849d4cd463e9d645ea96392a59ac148c073e65e
Min Hee  Park <minheekangg@gmail.com>
2018-09-26 02:45:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,3 +10,8 @@ def batch_badge_creator(arr)
   end
   return list
 end
+
+def assign_rooms(arr)
+  new_arr = []
+  arr.each_with_index |name, idx|
+   new_arr << "Hello, #{name}! You'll be assigned to room #{idx+1}!"
