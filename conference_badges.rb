f488840ab205a009ded383d65c3452bbe970d170
Min Hee  Park <minheekangg@gmail.com>
2018-09-26 02:47:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,6 @@ def assign_rooms(arr)
   new_arr = []
   arr.each_with_index |name,idx|
    new_arr << "Hello, #{name}! You'll be assigned to room #{idx+1}!"
- end
+  end
  return new_arr
- end
  end
